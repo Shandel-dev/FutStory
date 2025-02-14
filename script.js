df741971b678cf66eb5cfe05a2e2ca5f6085cf72
Shandel-dev <shandelvm16@gmail.com>
2025-02-14 14:31:20 -0300

    themas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const swiperBanner = new Swiper('.swiper-banner', {
-    direction: 'horizontal', // Alterado para horizontal
+    direction: 'horizontal',
 
     pagination: {
         el: '.swiper-pagination',
@@ -15,4 +15,23 @@ const swiperBanner = new Swiper('.swiper-banner', {
     }
 })
 
-const btn_theme = document.getElementById("")
+const theme = document.getElementById("lbl_theme")
+
+theme.addEventListener("click", (e) =>{
+    const check_theme = document.getElementById("input_tema")
+    const main = document.querySelector("main")
+
+    if(check_theme.checked){
+        theme.innerHTML = "<i class='fa-solid fa-futbol'></i>"
+        check_theme.checked = false
+        main.style.backgroundColor = "var(--cor-primaria)"
+        main.style.color = "black"
+    } else{
+        theme.innerHTML = "<i class='fa-solid fa-moon'></i>"
+        check_theme.checked = true
+        
+        main.style.color = "var(--cor-primaria)"
+        main.style.backgroundColor = "black"
+    }
+
+})
