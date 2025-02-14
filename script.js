674f73030d2e35bef7f6560453477955deb9b105
Shandel-dev <shandelvm16@gmail.com>
2025-02-14 16:47:30 -0300

    temas e conteudo lorem

--- script.js
+++ script.js
@@ -20,18 +20,25 @@ const theme = document.getElementById("lbl_theme")
 theme.addEventListener("click", (e) =>{
     const check_theme = document.getElementById("input_tema")
     const main = document.querySelector("main")
+    const aside = document.querySelector("aside")
 
-    if(check_theme.checked){
-        theme.innerHTML = "<i class='fa-solid fa-futbol'></i>"
-        check_theme.checked = false
-        main.style.backgroundColor = "var(--cor-primaria)"
-        main.style.color = "black"
-    } else{
-        theme.innerHTML = "<i class='fa-solid fa-moon'></i>"
-        check_theme.checked = true
+    if(check_theme.checked){    //mudar para tema claro
+
+        mudarTema("<i class='fa-solid fa-futbol'></i>", false, "var(--cor-dark-primaria)")
         
-        main.style.color = "var(--cor-primaria)"
-        main.style.backgroundColor = "black"
+        main.style.backgroundColor = "var(--cor-primaria)"
+        aside.style.backgroundColor = "var(--cor-terciaria)"
+        
+    } else{     //mudar para tema escuro
+        mudarTema("<i class='fa-solid fa-moon'></i>", true, "var(--cor-primaria")
+        main.style.backgroundColor = "var(--cor-dark-primaria)"
+        aside.style.backgroundColor = "var(--cor-dark-secundaria)"
     }
 
+    function mudarTema(icon, tema, color){
+        theme.innerHTML = icon
+        check_theme.checked = tema
+        main.style.color = color
+        aside.style.color = color
+    }
 })
