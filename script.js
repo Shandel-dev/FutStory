f6b3fde4fb93fbb89063ae19c6c55f4e9a053fb4
Shandel <shandelvm16@gmail.com>
2025-02-16 18:40:10 -0300

    update

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ const swiperBanner = new Swiper('.swiper-banner', {
     },
     autoplay: {
         delay: 10000,
-    }
+    },
+    loop: true,
 })
 
 const theme = document.getElementById("lbl_theme")
@@ -21,24 +22,21 @@ theme.addEventListener("click", (e) =>{
     const check_theme = document.getElementById("input_tema")
     const main = document.querySelector("main")
     const aside = document.querySelector("aside")
+    const linksaside = document.querySelector(".aside_links a")
+
+    main.classList.toggle("main-dark")
+    aside.classList.toggle("aside-dark")
+
+    
 
     if(check_theme.checked){    //mudar para tema claro
-
-        mudarTema("<i class='fa-solid fa-futbol'></i>", false, "var(--cor-dark-primaria)")
-        
-        main.style.backgroundColor = "var(--cor-primaria)"
-        aside.style.backgroundColor = "var(--cor-terciaria)"
-        
+        mudarTema("<i class='fa-solid fa-futbol'></i>", false)
     } else{     //mudar para tema escuro
-        mudarTema("<i class='fa-solid fa-moon'></i>", true, "var(--cor-primaria")
-        main.style.backgroundColor = "var(--cor-dark-primaria)"
-        aside.style.backgroundColor = "var(--cor-dark-secundaria)"
+        mudarTema("<i class='fa-solid fa-moon'></i>", true)
     }
 
-    function mudarTema(icon, tema, color){
+    function mudarTema(icon, theme_status){
         theme.innerHTML = icon
-        check_theme.checked = tema
-        main.style.color = color
-        aside.style.color = color
+        check_theme.checked = theme_status
     }
 })
